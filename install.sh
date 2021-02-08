f458f4a4150e8a5af3525a952d81ec67e69f8522
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-02-08 16:31:09 +0100

    Fix script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 echo "Setting up your Mac..."	
 
 # Check for Homebrew and install if we don't have it	
@@ -11,6 +10,8 @@ fi
 # Update Homebrew recipes	
 brew update
 
+brew tap homebrew/bundle	
+brew bundle
 
 # Set default MySQL root password and auth type.	
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"	
