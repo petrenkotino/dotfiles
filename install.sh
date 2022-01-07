6e76dc2b4f5ad82484693fabf8f66f98963cf588
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-01-07 10:21:51 +0100

    Update install script to run brew commands after installtion before using brew

--- install.sh
+++ install.sh
@@ -9,15 +9,22 @@ fi
 
 # Check for Homebrew and install if we don't have it	
 if test ! $(which brew); then	
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"	
-fi	
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes	
 brew update
 
 # Install all the dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle	
-brew bundle
+brew bundle --file $DOTFILES/Brewfile
 
 # Set default MySQL root password and auth type.	
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"	
