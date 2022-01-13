6282e10f2f70fa3af872091e3165ef2595f94941
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-01-13 09:04:18 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -8,6 +8,5 @@ SITES=$HOME/Sites
 git clone git@github.com:christophrumpel/laravelcoreadventures.git  $SITES/laravelcoreadventures
 git clone git@github.com:christophrumpel/christoph-rumpel.com.git $SITES/christophrumpel
 git clone git@github.com:christophrumpel/masteringphpstorm.git $SITES/masteringphpstorm
-git clone git@github.com:tatundkraft/onyx-jobboard.git $SITES/Clients/tatundkraft/onyx.jobboard
 git clone git@github.com:christophrumpel/larastreamers.git $SITES/larastreamers
 
