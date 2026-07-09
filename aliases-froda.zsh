651fd5687f0de68e61ba5858bed999c89f44e0ab
Konstantin Petrenko <petrenkotino@gmail.com>
2026-07-09 09:02:14 +0200

    add froda aliases

---
+++ aliases-froda.zsh
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------------#
+#	*START* GOTO
+#-----------------------------------------------------------------------------------#
+
+alias frodadev="cd ~/dev/froda-development"
+alias infra="cd ~/dev/froda-development/infrastructure"
+alias az-infra="cd ~/dev/froda-development/az-infrastructure"
+alias kuberes="cd ~/dev/froda-development/kubernetes-resources"
+alias kubedep="cd ~/dev/froda-development/kubernetes-deployments"
+alias dora="cd ~/dev/froda-development/dora-collector"
+alias devportal="cd ~/dev/froda-development/developer-portal"
+
+#-----------------------------------------------------------------------------------#
+#	*END* GOTO
+#-----------------------------------------------------------------------------------#
